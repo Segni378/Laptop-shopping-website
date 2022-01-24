5116026b1745d702b1b09186b800022053344d0a
Segni Guta <segniworku1992@gmail.com>
2022-01-24 14:16:21 +0300

    fixed few things on the home page

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -12,3 +12,17 @@ for (let i = 0; i < toggleFilter.length; i++) {
     filters.classList.toggle("deactive");
   });
 }
+
+// product detail expand
+
+const showDetails = document.querySelectorAll(".show-details");
+
+console.log(showDetails);
+for (let i = 0; i < showDetails.length; i++) {
+  showDetails[i].addEventListener("click", function() {
+    var parentElement = showDetails[i].parentElement;
+    var productDetails = parentElement.querySelector(".product-details");
+    productDetails.classList.toggle("active");
+    showDetails[i].classList.toggle("expand-rotate");
+  })
+}
--- public/utility/products.php
+++ public/utility/products.php
@@ -12,17 +12,37 @@
                     <img src="<?php echo $products[$i]["img"] ?>" alt="<?php $products[$i]["alt"] ?>">
             </div>
             <div class="sample-grid-display-products-description">
-                <p><?php echo $products[$i]["p1"] ?></p>
+                <p class="product-title"><?php echo $products[$i]["p1"] ?></p>
                 <p><?php echo $products[$i]["p2"] ?></p>
                 <p><?php echo $products[$i]["p3"] ?></p>
             </div>
             <div class="product-details">
-                <p><?php echo $products[$i]["p4"] ?></p> 
-                <p><?php echo $products[$i]["p5"] ?></p>
-                <p><?php echo $products[$i]["p6"] ?></p>
-                <p><?php echo $products[$i]["p7"] ?></p>
-                <p><?php echo $products[$i]["p8"] ?></p>
-                <p><?php echo $products[$i]["p9"] ?></p>            
+                <div >
+                    <i class="fab fa-windows" style="margin-left: 5px; margin-right: 5px; "></i>
+                    <p style="display:inline;"><?php echo $products[$i]["p4"] ?></p> 
+                </div>
+                <div>
+                    <i class="fas fa-photo-video" style="margin-left: 5px; margin-right: 5px;"></i>
+                    <p style="display:inline;"><?php echo $products[$i]["p5"] ?></p>
+                </div>
+                <div>
+                    <i class="fas fa-memory" style="margin-left: 5px; margin-right: 5px;"></i>
+                    <p style="display:inline;"></p><?php echo $products[$i]["p6"] ?></p>
+                </div>
+                <div>
+                    <i class="fas fa-hdd" style="margin-left: 5px; margin-right: 5px;"></i>
+                    <p style="display:inline;"><?php echo $products[$i]["p7"] ?></p>
+
+                </div>
+                <div>
+                    <i class="fas fa-laptop" style="margin-left: 5px; margin-right: 5px;"></i>
+                    <p style="display:inline;"><?php echo $products[$i]["p8"] ?></p>
+                </div>
+                <div>
+                    <i class="fas fa-weight" style="margin-left: 5px; margin-right: 5px;"></i>
+                     <p style="display:inline;"><?php echo $products[$i]["p9"] ?></p>
+                </div>
+                           
             </div>
             <a href="javascript:void(0)" class="show-details"></a>
             <div class="add-to-cart-btn">
