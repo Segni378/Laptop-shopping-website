88c1d8884e76aba721882a7e5327bec73eef0bd3
Segni Guta <segniworku1992@gmail.com>
2022-01-27 17:50:57 +0300

    add some javascript effects

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -17,7 +17,6 @@ for (let i = 0; i < toggleFilter.length; i++) {
 
 const showDetails = document.querySelectorAll(".show-details");
 
-console.log(showDetails);
 for (let i = 0; i < showDetails.length; i++) {
   showDetails[i].addEventListener("click", function() {
     var parentElement = showDetails[i].parentElement;
@@ -26,3 +25,54 @@ for (let i = 0; i < showDetails.length; i++) {
     showDetails[i].classList.toggle("expand-rotate");
   })
 }
+
+/* Revealing the brands section slowly on window load */
+
+const availableBrands = document.querySelector(".cat-available-brands").querySelectorAll("a");
+const availableSizes = document
+  .querySelector(".cat-available-size")
+  .querySelectorAll("a");
+console.log(availableSizes);
+window.addEventListener("load", () => {
+    for(let i = 0; i < availableBrands.length; i++) {
+      availableBrands[i].classList.add("active");
+    }
+    for(let i = 0; i < availableSizes.length; i++) {
+      availableSizes[i].classList.add("active");
+    }
+})
+
+/* Revealing sections on scroll */
+
+const sections = document.querySelectorAll(".reveal-slowly");
+
+const reveal = (entries, observer) => {
+  const [entry] = entries;
+  if(entry.isIntersecting){
+
+    const childrenOfTarget = entry.target.querySelectorAll(".content");
+   
+    childrenOfTarget.forEach(child => {
+      child.classList.remove("hide--section");
+    })
+  
+    observer.unobserve(entry.target);
+  }
+  
+}
+const observeOptions = {
+  root: null,
+  threshold: 0.5 
+}
+
+const sectionObserver = new IntersectionObserver(reveal, observeOptions);
+
+sections.forEach(section => {
+
+  sectionObserver.observe(section);
+  const allChild = section.querySelectorAll(".content");
+  allChild.forEach(child  => {
+    child.classList.add("hide--section");
+  })
+  
+});
