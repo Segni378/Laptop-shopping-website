f8ac5d399e4765fa7ecc547f009351f6ce1a203f
Segni Guta <segniworku1992@gmail.com>
2022-01-30 23:02:03 +0300

    add footer

---
+++ public/utility/footer.php
@@ -0,0 +1,34 @@
+<footer>
+        <div class = "footer-section">
+            <h5>ABOUT</h5>
+            <ul>
+                <a href="#"><li>Contact Us</li></a>
+                <a href="#"><li>About Us</li></a>
+                <a href="#"><li>Awards and higlights</li></a>
+            </ul>
+        </div>
+        <div>
+            <h5>HELP</h5>
+            <ul>
+                <a href="#"><li>Returns and Complaints</li></a>
+                <a href="#"><li>Questions and Answers</li></a>
+                <a href="#"><li>Terms of use</li></a>
+                <a href="#"><li>Privacy Policy</li></a>
+            </ul>
+        </div>
+        <div>
+            <h5>OUR ACCOUNT</h5>
+            <ul>
+                <a href="#"><li>My Account</li></a>
+                <a href="#"><li>My Order</li></a>
+                <a href="#"><li>My Wishlist</li></a>
+            </ul>
+        </div>
+        <div>
+            <h5>INFORMATION</h5>
+            <ul>
+                <a href="#"><li>Warranty Guide</li></a>
+                <a href="#"><li>Feed back</li></a>
+            </ul>
+        </div>
+ </footer>
